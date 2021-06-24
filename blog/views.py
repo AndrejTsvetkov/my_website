0687a653b22c0a5303804d13db37a5059cc5add3
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2021-06-24 22:32:27 +0300

    Created base template with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'TsvetkovAA',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'June 22, 2021'
+    },
+    {
+        'author': 'SinyavinDS',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 23, 2021'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
