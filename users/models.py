2b895f291dfc64f2727f9499228f53c468e20828
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2021-10-01 17:40:03 +0300

    Added deletion of old profile pictures

--- users/models.py
+++ users/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from model_utils import FieldTracker
+import os
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    tracker = FieldTracker()
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
     def save(self, *args, **kwargs):
+        if self.tracker.has_changed('image'):
+            if os.path.isfile(self.tracker.previous('image').path):
+                os.remove(self.tracker.previous('image').path)
         super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
