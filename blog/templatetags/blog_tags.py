d815ca428652ce48b3f164bb27c6dd9fb3592596
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2022-01-29 21:20:43 +0300

    Added the most popular posts to the sidebar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,3 +7,10 @@ register = template.Library()
 @register.simple_tag()
 def get_recent_posts(num=3):
     return Post.objects.all().order_by('-date_posted')[:num]
+
+
+@register.simple_tag()
+def get_popular_posts(num=3):
+    queryset = Post.objects.all()
+    queryset = sorted(queryset, key=lambda i: i.comments.count(), reverse=True)
+    return queryset[:num]
