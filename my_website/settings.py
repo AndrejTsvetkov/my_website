0f2ea5645108b2daf6db6815f0f9bdfc77d3ad82
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2022-02-02 22:58:57 +0300

    Added AWS S3 for file uploads (only for dev)

--- my_website/settings.py
+++ my_website/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'django.contrib.humanize',
-
-    'rest_framework'
+    'rest_framework',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -158,6 +158,18 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'eu-north-1'
 
 # Override production variables if DJANGO_DEVELOPMENT env variable is set
 if os.environ.get('DJANGO_DEVELOPMENT'):
--- users/models.py
+++ users/models.py
@@ -13,17 +13,17 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        # Issue with deleting the default profile pics
-        if self._state.adding is False:  # It means object is being updated not created
-            if self.tracker.has_changed('image') and self.tracker.previous('image').name != "default.jpg":
-                if os.path.isfile(self.tracker.previous('image').path):
-                    os.remove(self.tracker.previous('image').path)
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # def save(self, *args, **kwargs):
+    #     # Issue with deleting the default profile pics
+    #     if self._state.adding is False:  # It means object is being updated not created
+    #         if self.tracker.has_changed('image') and self.tracker.previous('image').name != "default.jpg":
+    #             if os.path.isfile(self.tracker.previous('image').path):
+    #                 os.remove(self.tracker.previous('image').path)
+    #     super().save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.path)
+    #
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
