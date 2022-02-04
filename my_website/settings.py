555f684ed8d74863f626b79dbdf591f96a97c537
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2022-02-04 20:08:27 +0300

    Changes in settings

--- my_website/settings.py
+++ my_website/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/vol/web/static'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/vol/web/media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = '/vol/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -152,6 +152,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
+# Sending emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -159,18 +160,19 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_REGION_NAME = 'eu-north-1'
-
 # Override production variables if DJANGO_DEVELOPMENT env variable is set
+# We use AWS S3 only for production, for development we use standard Media and Static settings from settings_dev.py
 if os.environ.get('DJANGO_DEVELOPMENT'):
     from .settings_dev import DEBUG, SECRET_KEY, ALLOWED_HOSTS, STATIC_URL, MEDIA_URL, MEDIA_ROOT, DATABASES
+else:
+    # AWS Storage
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_S3_REGION_NAME = 'eu-north-1'
