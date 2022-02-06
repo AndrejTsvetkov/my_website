0d74efa77ca7fefa6eab42c075bbdf389de17482
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2022-02-06 21:42:15 +0300

    Added a custom storage for media files

--- my_website/settings.py
+++ my_website/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'django.contrib.humanize',
     'rest_framework',
-    'storages'
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -170,9 +170,9 @@ else:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
-
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_PUBLIC_MEDIA_LOCATION = os.environ.get('AWS_PUBLIC_MEDIA_LOCATION')
+    DEFAULT_FILE_STORAGE = 'core.custom_storage.PublicMediaStorage'
     AWS_S3_SIGNATURE_VERSION = 's3v4'
     AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_S3_ADDRESSING_STYLE = "virtual"
+
